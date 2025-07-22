3585c23790a2ce49692d7b686651cf85a71a35ca
pranab56 <pronabhalder53@gmail.com>
2025-07-22 12:58:20 +0600

    add delete account page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+ 
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "31.97.98.240",
+    // port: 4174,
+  },
+  build: {
+    sourcemap: false,  // disables source maps for production
+    minify: "esbuild", // faster minifier
+  },
+});  
