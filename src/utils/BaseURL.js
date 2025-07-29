346dfb1a241b4796c8b322ef3ea7e865655a16b4
pranab56 <pronabhalder53@gmail.com>
2025-07-29 10:02:56 +0600

    all ok

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,5 +1,5 @@
 // export const baseURL = "http://10.10.7.109:5005";
 
-export const baseURL = "http://31.97.98.240:5005";
+// export const baseURL = "http://31.97.98.240:5005";
 
-// export const baseURL = "https://rabby3000.binarybards.online";
+export const baseURL = "http://31.97.98.240:5005";
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
- 
+import { defineConfig } from "vite";
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "31.97.98.240",
+    // host: "31.97.98.240",
+    host: "0.0.0.0",
     // port: 4174,
   },
   build: {
